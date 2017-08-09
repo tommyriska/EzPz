a17e6770b3d2a891a66bdd70c4f1054c9f4b6b58
tommyriska <tommy.riska@icloud.com>
2017-08-09 02:24:43 +0200

    Started implementing the networking part of the program. Also, UI is starting to get in shape but needs some more work along the way. Added the c.py just to confirm connection between two sockets.

--- ezpz.py
+++ ezpz.py
@@ -1,5 +1,8 @@
 import socket
 import click
+import time
+import random
+import os
 
 @click.group()
 def cli():
@@ -24,25 +27,41 @@ def menu():
             if char == '1':
                 click.clear()
                 printbanner()
-                click.echo('\n\nFILE RECIEVED')
+                reciever()
+                #click.echo('\n\nFILE RECIEVED')
+                #click.echo('\nb: back')
+                #char = click.getchar()
+                #if char == 'b':
+                #    menu = 'main'
+                #else:
+                #    click.echo('Invalid input')
+            elif char == '2':
+                click.clear()
+                printbanner()
+                sender()
+                click.pause
+                #click.echo('\n\nFILE SENT!')
+                #click.echo('\nb: back')
+                #char = click.getchar()
+                #if char == 'b':
+                #    menu = 'main'
+                #else:
+                #    click.echo('Invalid input')
+            elif char == '3':
+                click.clear()
+                printbanner()
+                click.echo('INSTRUCTIONS:')
+                click.echo("""
+    1. Reciever selects menu option #2.
+    2. Sender selects menu option #1.
+    3. Sender chooses a file to send.
+        """)
                 click.echo('\nb: back')
                 char = click.getchar()
                 if char == 'b':
                     menu = 'main'
                 else:
                     click.echo('Invalid input')
-            elif char == '2':
-                click.clear()
-                printbanner()
-                click.echo('\n\nFILE SENT!')
-                char = click.getchar()
-                if char == 'b':
-                    menu = 'main'
-                else:
-                    click.echo('Invalid input')
-            elif char == '3':
-                click.clear()
-                printbanner()
             elif char == '4':
                 click.clear()
                 printbanner()
@@ -69,12 +88,33 @@ _________________________________________________________________________
 |                    |______/___|_|   /___|                             |
 |_______________________________________________________________________|""",fg='green', bg='black'))
 
-@cli.command()
+
 def sender():
     """THIS ACTS AS THE SERVER"""
+    host = ''
+    port = click.prompt(text='Choose a port between 10000-30000', default=1337, confirmation_prompt=True, show_default=True)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen(1)
+        click.echo('Setup done, waiting for connection..')
+        conn, addr = s.accept()
+        with conn:
+            click.echo('%s connected to the server' % conn.getpeername)
+            msg = conn.recv(1024)
+            if not msg:
+                click.echo('No message returned, exiting..')
+                return
+            else:
+                click.echo(msg)
+            click.pause()
+            os._exit(1)
 
 @cli.command()
 def reciever():
     """THIS ACTS AS THE CLIENT"""
+    printbanner()
+
+
+
 if __name__ == '__main__':
     cli()
